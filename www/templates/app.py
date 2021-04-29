5d8612ea32f2843f9385cd0918b94dd2e629afb3
yutonghao <yutonghao0887@wezhuiyi.com>
2021-04-29 11:51:47 +0800

    2.app框架搭建

---
+++ www/templates/app.py
@@ -0,0 +1,24 @@
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio
+import os
+import json
+import time
+from datetime import datetime
+
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+
+loop = asyncio.get_event_loop() #生成loop对象
+loop.run_until_complete(init(loop))
+loop.run_forever()
